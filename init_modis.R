03db8b6ae25e2eaabef233d7d4ef6845058c2946
Flugmango <borisstoecker@gmx.de>
2019-05-23 19:41:13 +0200

    bfastspatial + reclassification init

--- init_modis.R
+++ init_modis.R
@@ -1,16 +1,17 @@
 #init_modis
-
 #load dependencies
 library(devtools)
 # install_github('loicdtx/bfastSpatial')
+# install.packages("greenbrown", repos="http://R-Forge.R-project.org")
 library(bfastSpatial)
+library(greenbrown)
 library(rgdal)
 library(MODIS)
 library(mapview)
 library(bfastSpatial)
 library(Kendall)
 
-setwd("C:/Users/Boris/Documents/MODIS_Windfarm_Temperature")
+setwd("C:/Users/Flugmango/Documents/MODIS_Windfarm_Temperature")
 source("locmodisviewtime_LocST.R")
 #preparation
 lap = "./MODIS"
@@ -23,14 +24,20 @@ MODISoptions(localArcPath = lap, outDirPath = odp)
 # bbox creation
 bbox= extent(138.858948, 139.113693, -34.355908, -33.966142)
 #Download Data
-runGdal( product="MOD11A1", extent = bbox, begin="2019137", end="2019143", SDSstring="1010101", outProj="4326")
+runGdal( product="MOD11A1", extent = bbox, begin="2001001", end="2018365", SDSstring="1010101", outProj="4326")
 
 # runGdal( product="MOD11A1", tileH = 29, tileV = 12, begin="2000001", end="2018365", SDSstring="10001", outProj="4326")
 # runGdal( product="MOD11A1", tileH = 29, tileV = 12, begin=as.Date("2000-1-1"), end=as.Date("2018-12-31"), SDSstring="10001", outProj="4326")
 
 # processing data
-files <- list.files(path="./MODIS/PROCESSED/MOD11A1.006_20190314172848", pattern="*.tif$", full.names=TRUE)
+files <- list.files(path="./MODIS/PROCESSED/MOD11A1.006_2000_2018", pattern="*.tif$", full.names=TRUE)
+# ~ 5mins
 stack = stack(files)
+end_time <- Sys.time()
+#reclassify files for frost-/no-frost-events
+# reclassify the values into two groups: below 1°C = 1, higher than 1°C = 0
+rc <- reclassify(stack, c(-Inf,1,1, 1,Inf,0))
+
 
 # saving
 save(stack, file="./temp/terra_stack.RData")
@@ -46,41 +53,29 @@ load("./temp/terra_stack_celsius.RData")
 
 
 #bfast Analysis
-
+# 5 minutes
 bfast_stack <- timeStackMODIS(files)
+# 1.5 hours
 bfast_stack_celsius <- bfast_stack / 50 - 273.15
 
 save(bfast_stack_celsius, file="./temp/terra_bfast_stack_celsius.RData")
 load("./temp/terra_bfast_stack_celsius.RData")
 
-start <- Sys.time()
 summary_stack <- summaryBrick(bfast_stack_celsius, fun=mean, na.rm=TRUE)
-end <- Sys.time()
 save(summary_stack, file="./temp/summary_stack.RData")
 load("./temp/summary_stack.RData")
+mapview(summary_stack)
 
-#######adding time vector
+#######adding time vector on hold
 
 #converting LST to LT
-fnam <- "./MODIS/MODIS/MOD11A1.006/MOD11A1.A2019137.h29v12.006.2019138085521.hdf"
-ra <- c(138.858948, 139.113693, -34.355908, -33.966142)
-viewtime <- raster("./MODIS/PROCESSED/MOD11A1.006_20190521001332/MOD11A1.A2019137.Day_view_time.tif")
-ltz <- "Australia/Adelaide"   # https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
-newproj <- "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"
-
-locmodisviewtime(fnam, viewtime, ra, ltz, newproj)
-
-
-
-
-
-
-#testing date detection, doesnt work, dates vector needs to be supplied
-# start <- Sys.time()
-# med_03_08_test <- summaryBrick(bfast_stack_celsius, fun=median, na.rm=TRUE, minDate="2003-01-01", 
-#                             maxDate="2008-12-31")
-# end <- Sys.time()
-
+# fnam <- "./MODIS/MODIS/MOD11A1.006/MOD11A1.A2019137.h29v12.006.2019138085521.hdf"
+# ra <- c(138.858948, 139.113693, -34.355908, -33.966142)
+# viewtime <- raster("./MODIS/PROCESSED/MOD11A1.006_20190521001332/MOD11A1.A2019137.Day_view_time.tif")
+# ltz <- "Australia/Adelaide"   # https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+# newproj <- "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"
+# 
+# locmodisviewtime(fnam, viewtime, ra, ltz, newproj)
 
 
 #Kendall analysis
